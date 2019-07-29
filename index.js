a92c5a1c8fc2d1a7d65cb2883217da69a748330f
Acassio Marques <ajmarques@uolinc.com>
2019-07-29 11:22:06 -0300

    add multiple functions support

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ class ServerlessPlugin {
     constructor(serverless, options) {
 
         this.serverless = serverless;
-        this.props = this.serverless.service.custom.encryptenv
+        this.props = this.serverless.service.custom.env
         this.serverless.service.provider.environment = this.serverless.service.provider.environment || {}
         this.awsConfig = {region: this.serverless.service.provider.region}
         this.ssm = new AWS.SSM(this.awsConfig);
         this.kms = new AWS.KMS(this.awsConfig);
+        this.global = !this.props.functions
 
         this.hooks = {
             'before:deploy:createDeploymentArtifacts': this.beforeDeploy.bind(this)
@@ -20,7 +21,6 @@ class ServerlessPlugin {
     }
 
     beforeDeploy() {
-
         return new Promise((resolve) => {
             return this.encryptSSM().then(data => {
                 resolve(data)
@@ -28,11 +28,13 @@ class ServerlessPlugin {
         })
     }
 
-    async encryptSSM() {
+    resolveValues(variables){
 
         return new Promise(async (resolve, reject) => {
 
-            this.props.variables.forEach((k) => {
+            var list = []
+
+            await this.asyncForEach(variables,async (k,i) => {
 
                 let key;
 
@@ -40,24 +42,82 @@ class ServerlessPlugin {
                     key = prop
                 }
 
+                if(k[key].indexOf("encrypt:") === -1 && k[key].indexOf("ssm:") === -1 ){
+                    list.push({key,value:k[key]})
+                    return
+                }
+
+                let isEncrypted = k[key].indexOf("encrypt:") !== -1
+                let keyName = k[key].replace("${self:provider.stage}",this.serverless.service.provider.stage)
+                    .replace("encrypt:","")
+                    .replace("ssm:","")
+
                 let params = {
-                    Name: k[key].replace("${self:provider.stage}",this.serverless.service.provider.stage),
-                    WithDecryption: true
+                    Name: keyName,
+                    WithDecryption:isEncrypted
                 };
 
-                this.ssm.getParameter(params,  (err, data)=> {
-                    if (err) {
-                        console.log(err, err.stack);
-                        reject(err)
-                    } else {
-                        let cipherText = data.Parameter.Value
-                        this.encrypt(cipherText).then(encrypted=>{
-                            this.serverless.service.provider.environment[key] = encrypted
-                            resolve(true)
-                        })
-                    }
+                await this.getParameter(params,isEncrypted).then(value=>{
+                    list.push({key,value})
+                }).catch(err=>{
+                    reject(err)
                 });
+
             })
+            resolve(list)
+        })
+    }
+
+    async getParameter(params,encrypt){
+        return new Promise(async (resolve, reject) => {
+            await this.ssm.getParameter(params,  async(err, data)=> {
+                if (err) {
+                    console.log(err, err.stack);
+                    reject(err)
+                } else {
+                    let cipherText = data.Parameter.Value
+                    if(!encrypt){
+                        resolve(data.Parameter.Value)
+                        return
+                    }
+                    await this.encrypt(cipherText).then(value=>{
+                        resolve(value)
+                    })
+                }
+            });
+
+        })
+    }
+
+    async asyncForEach(array, callback) {
+        for (let index = 0; index < array.length; index++) {
+            await callback(array[index], index, array);
+        }
+    }
+
+    async encryptSSM() {
+
+        return new Promise(async (resolve, reject) => {
+
+            if (this.global){
+                await this.resolveValues(this.props.variables).then(result=>{
+                    result.forEach(i=>{
+                        this.serverless.service.provider.environment[i.key] = i.value
+                    })
+                    resolve(true)
+                })
+                return
+            }
+            await this.asyncForEach(this.props.functions,async(f)=>{
+                this.serverless.service.functions[f.name].environment = this.serverless.service.functions[f.name].environment || {}
+                await this.resolveValues(f.variables).then(result=>{
+                    result.forEach(v=>{
+                        this.serverless.service.functions[f.name].environment[v.key] = v.value
+                    })
+                })
+            })
+
+            resolve(true)
 
         })
     }
